93cec761bb338df5a9bf5909f56f2d066b2df629
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2021-06-20 01:36:31 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -5,20 +5,23 @@ masuk="y"
 while [ $masuk = "y" ]
 do
   clear
-  echo "            \033[1;32m  Cpu Miner Installer\n\n"
-  echo -n "Apakah anda ingin melanjutkan untuk memasang Cpu Miner? Default Y (Y/N) : "
+  echo "\033[5;32m  Cpu Miner Installer\n\n"
+  echo -n "\e[0m \x1b[38;5;87mApakah anda ingin melanjutkan untuk memasang Cpu Miner? \n Default Y (Y/N) : "
   read yes;
   if [ $yes = "y" ]||[ $yes = "Y" ]
   then
       echo "Sedang menginstall Cpu Miner....!"
       apt-get update && apt-get upgrade -y
       apt-get install wget -y
+      echo "\e[0m \x1b[38;5;87mPenginstalan wget secara otomatis."
       mkdir minerd
       cd minerd
       wget https://github.com/ibed-berto/m-minerd/raw/main/pool01/cpuminer-linux.tar.gz 
+      echo "\e[0m \x1b[38;5;87mSedang mendownload Cpu Miner. Pastikan koneksi internet anda bagus !!!"
       tar xf cpuminer-linux.tar.gz
+      echo "\e[0m \x1b[38;5;87mSedang mengexstact file"
       rm cpuminer-linux.tar.gz -rf
-      echo "Penginstalan Cpu Miner selesai"
+      echo "\e[0m \x1b[38;5;87mPenginstalan Cpu Miner selesai"
       exit
   elif [ $yes = "n" ]||[ $yes = "N" ]
   then
