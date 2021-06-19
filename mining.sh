d46030108b34ce43e59972d14c34c8770a5a85cf
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2021-06-20 02:43:53 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-aku = Cpu Miner
+aku=Cpu Miner
 masuk="y"
 while [ $masuk = "y" ]
 do
   clear
-  printf "\033[5;32m$aku Installer\n\n"
+  printf "\033[5;32m[${aku}] Installer\n\n"
   echo -n "\e[38;5;87mApakah anda ingin melanjutkan untuk memasang Cpu Miner?\nDefault Y (Y/N) :\n "
   read yes;
   if [ $yes = "y" ]||[ $yes = "Y" ]
   then
-      printf "Sedang menginstall $aku....\n"
+      printf "Sedang menginstall ....\n"
       mkdir minerd
       cd minerd
       printf "\e[38;5;87mSedang mendownload $aku. Pastikan koneksi internet anda baik!!!\n"
       wget https://github.com/ibed-berto/m-minerd/raw/main/pool01/cpuminer-linux.tar.gz 
-      printf " Download $aku selesai!\n"
+      printf " Download {$aku} selesai!\n"
       tar xf cpuminer-linux.tar.gz
       echo "\e[38;5;87mSedang mengexstact file\n"
       rm cpuminer-linux.tar.gz -rf
