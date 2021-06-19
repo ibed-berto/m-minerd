630675ea616b4f712cc3dbad820cdc5b3273704e
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2021-06-20 02:31:23 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-
+aku = Cpu Miner
 masuk="y"
 while [ $masuk = "y" ]
 do
   clear
-  echo "\033[5;32mCpu Miner Installer\n\n"
+  printf "\033[5;32m$aku Installer\n\n"
   echo -n "\e[38;5;87mApakah anda ingin melanjutkan untuk memasang Cpu Miner?\nDefault Y (Y/N) :\n "
   read yes;
   if [ $yes = "y" ]||[ $yes = "Y" ]
   then
-      printf "Sedang menginstall Cpu Miner....\n"
+      printf "Sedang menginstall $aku....\n"
       mkdir minerd
       cd minerd
-      printf "\e[38;5;87mSedang mendownload Cpu Miner. Pastikan koneksi internet anda baik!!!\n"
+      printf "\e[38;5;87mSedang mendownload $aku. Pastikan koneksi internet anda baik!!!\n"
       wget https://github.com/ibed-berto/m-minerd/raw/main/pool01/cpuminer-linux.tar.gz 
-      printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;87m Download selesai!\n"
+      printf " Download $aku selesai!\n"
       tar xf cpuminer-linux.tar.gz
-      printf "\e[38;5;87mSedang mengexstact file\n"
+      echo "\e[38;5;87mSedang mengexstact file\n"
       rm cpuminer-linux.tar.gz -rf
-     printf "\e[38;5;87mPenginstalan Cpu Miner selesai\n"
+     printf "\e[38;5;87mPenginstalan $aku selesai\n"
       exit
 rm mining.sh
   elif [ $yes = "n" ]||[ $yes = "N" ]
