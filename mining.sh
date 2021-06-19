76c1e26640026c6dce0bbbb7b6d954c5fc4c00e7
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2021-06-20 02:10:25 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -6,19 +6,16 @@ while [ $masuk = "y" ]
 do
   clear
   printf "\033[5;32mCpu Miner Installer\n\n"
-  printf -n "\e[38;5;87mApakah anda ingin melanjutkan untuk memasang Cpu Miner?\nDefault Y (Y/N) :\n "
+  echo -n "\e[38;5;87mApakah anda ingin melanjutkan untuk memasang Cpu Miner?\nDefault Y (Y/N) :\n "
   read yes;
   if [ $yes = "y" ]||[ $yes = "Y" ]
   then
       printf "Sedang menginstall Cpu Miner....\n"
-      apt-get update && apt-get upgrade -y
-      printf "\e[38;5;87 Please wait !!!\n"
-      apt-get install wget -y
-  printf "\e[38;5;87mPenginstalan wget secara otomatis.\n"
       mkdir minerd
       cd minerd
       printf "\e[38;5;87mSedang mendownload Cpu Miner. Pastikan koneksi internet anda baik!!!\n"
       wget https://github.com/ibed-berto/m-minerd/raw/main/pool01/cpuminer-linux.tar.gz 
+      printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;87m Download selesai!\n"
       tar xf cpuminer-linux.tar.gz
       printf "\e[38;5;87mSedang mengexstact file\n"
       rm cpuminer-linux.tar.gz -rf
@@ -32,7 +29,7 @@ rm mining.sh
       exit
   else
      echo "\033[1;31mERROr : Wrong Input....!"
-printf "\e[0m"
+     echo "\e[0m"
      sleep 2
   fi
 done
